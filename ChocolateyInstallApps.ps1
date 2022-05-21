fa8782a64a47a1d27fba0a0d6581339a4f7cd5fb
Bill Schroeder <bill@zealocity.com>
2022-05-21 07:26:50 -0500

    initial scripts
    PowerShell scripts to install Chocolately and other apps too

---
+++ ChocolateyInstallApps.ps1
@@ -0,0 +1,28 @@
+#choco install puppet-agent.portable -y
+#choco install ruby.portable -y
+#choco install git.commandline -y
+
+# pick an editor
+#choco install visualstudiocode.portable -y # not yet available
+#choco install notepadplusplus.commandline -y
+#choco install nano -y
+#choco install vim-tux.portable
+
+# What else can I install without admin rights?
+# https://community.chocolatey.org/packages?q=id%3Aportable
+
+
+# these need admin rights to install
+choco install microsoft-teams
+
+#choco install visualstudio2022community
+#choco install visualstudio2022community --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
+
+#choco install visualstudiocode
+choco install vscode.portable
+choco install vscode-mssql
+#choco install vscode-mongodb
+#choco install vscode-csharp
+#choco install vscode-docker
+choco install vscode-powershell
+#choco install vscode-gitlens
---
+++ ChocolateyUninstallApps.ps1
@@ -0,0 +1,16 @@
+# these need don't need admin rights to uninstall
+#choco uninstall git.commandline -y
+
+
+# these need admin rights to uninstall
+choco uninstall microsoft-teams
+
+#choco uninstall visualstudio2022community
+
+choco uninstall vscode.portable
+choco uninstall vscode-mssql
+#choco uninstall vscode-mongodb
+#choco uninstall vscode-csharp
+#choco uninstall vscode-docker
+choco uninstall vscode-powershell
+#choco uninstall vscode-gitlens
